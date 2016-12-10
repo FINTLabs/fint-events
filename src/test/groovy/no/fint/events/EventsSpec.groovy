6d7a6df7798fe32033e7c07eb9876f869b8e00dc
jarlehansen <jarle@jarlehansen.net>
2016-12-10 14:53:42 +0100

    added tests for Events

--- src/test/groovy/no/fint/events/EventsSpec.groovy
+++ src/test/groovy/no/fint/events/EventsSpec.groovy
@@ -1,8 +1,10 @@
 package no.fint.events
 
+import no.fint.events.testutils.TestDto
 import org.springframework.amqp.core.*
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import spock.lang.Specification
 
 class EventsSpec extends Specification {
@@ -13,12 +15,14 @@ class EventsSpec extends Specification {
 
     void setup() {
         amqpAdmin = Mock(AmqpAdmin)
-        connectionFactory = Mock(ConnectionFactory)
+        connectionFactory = Mock(ConnectionFactory) {
+
+        }
         eventsRegistry = Mock(EventsRegistry)
         events = new Events(amqpAdmin: amqpAdmin, connectionFactory: connectionFactory, eventsRegistry: eventsRegistry)
     }
 
-    def "Create exchange and queues when registering a message listener"() {
+    def "Create exchange and queues when registering an unstarted message listener"() {
         when:
         def listener = events.registerUnstartedListener(new TopicExchange("my-exchange"), new Queue("my-queue"), MessageListener)
 
@@ -26,7 +30,19 @@ class EventsSpec extends Specification {
         1 * amqpAdmin.declareExchange(_ as Exchange)
         1 * amqpAdmin.declareQueue(_ as Queue)
         1 * amqpAdmin.declareBinding(_ as Binding)
-        1 * eventsRegistry.add(_ as String, _ as Class) >> Optional.of(new SimpleMessageListenerContainer())
+        1 * eventsRegistry.add(_ as String, _ as Class) >> Optional.of(Mock(SimpleMessageListenerContainer))
+        listener.isPresent()
+    }
+
+    def "Create exchange and queues when registering a message listener"() {
+        when:
+        def listener = events.registerListener("my-exchange", "my-queue", MessageListener)
+
+        then:
+        1 * amqpAdmin.declareExchange(_ as Exchange)
+        1 * amqpAdmin.declareQueue(_ as Queue)
+        1 * amqpAdmin.declareBinding(_ as Binding)
+        1 * eventsRegistry.add(_ as String, _ as Class) >> Optional.of(Mock(SimpleMessageListenerContainer))
         listener.isPresent()
     }
 
@@ -37,5 +53,30 @@ class EventsSpec extends Specification {
         then:
         1 * amqpAdmin.deleteQueue("my-queue") >> true
         1 * amqpAdmin.deleteExchange("my-exchange") >> true
+    }
+
+    def "Remove registered message listener"() {
+        when:
+        events.removeListener("my-exchange", "my-queue")
+
+        then:
+        1 * amqpAdmin.removeBinding(_ as Binding)
+        1 * eventsRegistry.close("my-queue")
+    }
+
+    def "Create rabbit template with message converter"() {
+        when:
+        def rabbitTemplate = events.rabbitTemplate(TestDto)
+
+        then:
+        rabbitTemplate.messageConverter instanceof Jackson2JsonMessageConverter
+    }
+
+    def "Create rabbit template with connection factory"() {
+        when:
+        def rabbitTemplate = events.rabbitTemplate()
+
+        then:
+        rabbitTemplate.connectionFactory != null
     }
 }
