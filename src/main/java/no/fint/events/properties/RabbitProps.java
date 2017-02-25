78332793eb88907614a54b2a1f2ed5217e123381
Frode Sjovatsen <frode@sjovatsen.net>
2017-02-25 14:56:17 +0100

    added code for cloud config

--- src/main/java/no/fint/events/properties/RabbitProps.java
+++ src/main/java/no/fint/events/properties/RabbitProps.java
@@ -2,7 +2,11 @@ package no.fint.events.properties;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
 
+@Component
+@RefreshScope
 @Getter
 public class RabbitProps {
 
