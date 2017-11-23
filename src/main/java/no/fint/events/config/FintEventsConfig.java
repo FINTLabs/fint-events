1ac277903b4ad44c705a09c57b2fcae195e9946b
Jarle Hansen <jarle@jarlehansen.net>
2017-11-23 07:35:32 +0100

    simplified spring config

--- src/main/java/no/fint/events/config/FintEventsConfig.java
+++ src/main/java/no/fint/events/config/FintEventsConfig.java
@@ -8,12 +8,9 @@ import no.fint.events.internal.EventDispatcher;
 import no.fint.events.internal.FintEventsHealth;
 import no.fint.events.internal.QueueType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.concurrent.BlockingQueue;
 
 @Configuration
 @ComponentScan(basePackageClasses = FintEvents.class)
@@ -30,27 +27,16 @@ public class FintEventsConfig {
         return new FintEvents(downstreamEventDispatcher(), upstreamEventDispatcher(), fintEventsHealth);
     }
 
-    @Qualifier("no.fint.events.downstream")
     @Bean
     public EventDispatcher downstreamEventDispatcher() {
-        return new EventDispatcher(downstreamQueue());
+        IQueue<Event> queue = hazelcastInstance.getQueue(QueueType.DOWNSTREAM.getQueueName());
+        return new EventDispatcher(queue);
     }
 
-    @Qualifier("no.fint.events.upstream")
     @Bean
     public EventDispatcher upstreamEventDispatcher() {
-        return new EventDispatcher(upstreamQueue());
-    }
-
-    @Bean
-    public BlockingQueue<Event> downstreamQueue() {
-        return hazelcastInstance.getQueue(QueueType.DOWNSTREAM.getQueueName());
-    }
-
-    @Bean
-    public BlockingQueue<Event> upstreamQueue() {
         IQueue<Event> queue = hazelcastInstance.getQueue(QueueType.UPSTREAM.getQueueName());
         queue.addItemListener(fintEventsHealth, true);
-        return queue;
+        return new EventDispatcher(queue);
     }
 }
